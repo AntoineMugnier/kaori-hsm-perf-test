ff07eec6b397085e032e01eb260c1d5c4f1af9e6
Antoine Mugnier <amugnier@europe.com>
2023-10-21 18:45:01 -0700

    few comments added to build.sh

--- qpcpp-test/build.sh
+++ qpcpp-test/build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#
+# This script builds the static lib. Must be run from its own dirctory
+
+# Create Build dir in which will be generated the Makefiles
 mkdir -p build
+
 cd build
 
-# setup cmake
+# Generate Makefiles
 cmake ../ -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=gcc-arm-none-eabi.cmake -DTOOLCHAIN_BIN_DIR=/opt/arm-gnu-toolchain-12.3.rel1-x86_64-arm-none-eabi/bin
+
+# Build project
 make
