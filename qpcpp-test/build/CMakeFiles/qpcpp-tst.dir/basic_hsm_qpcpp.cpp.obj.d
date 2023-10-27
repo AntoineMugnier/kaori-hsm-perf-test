4fb03079a8a86e9003b1a538f40353e0abdea657
Antoine Mugnier <amugnier@europe.com>
2023-10-26 18:28:47 -0700

    have added .gitignore to prevent qpcpp-test from being added to index in the future

--- qpcpp-test/build/CMakeFiles/qpcpp-tst.dir/basic_hsm_qpcpp.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/qpcpp-tst.dir/basic_hsm_qpcpp.cpp.obj: \
- /home/antoine/Documents/projects/kaori-hsm-perf-test/qpcpp-test/basic_hsm_qpcpp.cpp
--- qpcpp-test/build/CMakeFiles/qpcpp-tst.dir/tst.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/qpcpp-tst.dir/tst.c.obj: \
- /home/antoine/Documents/projects/kaori-hsm-perf-test/qpcpp-test/tst.c
